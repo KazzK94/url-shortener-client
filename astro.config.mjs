9866e1b840e8ccec19056b175e949f471cb7f5fc
Kazz <i94773007@gmail.com>
2025-03-18 19:34:15 +0100

    implemented basic authentication with clerk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify'
+import clerk from '@clerk/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify()
-});
+	integrations: [
+		clerk()
+	],
+	adapter: netlify(),
+	output: 'server'
+})
