7eefb5a08ed3c687dae0913fdcffe59f052f2b87
Kazz <i94773007@gmail.com>
2025-03-20 14:27:46 +0100

    fixed coment for specific clarification

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -4,10 +4,14 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
 /** 
  * Executes a fetch() requiring a mandatory token that should be obtained from the session. 
  * 
- * To obtain the token, follow these indications:
+ * To obtain the token from a React Component, follow these indications:
  * 1) Import Clerk's useAuth: `import { useAuth } from '@clerk/astro/react'`
  * 2) Call useAuth() to get the getToken() function: `const { getToken } = useAuth()`
  * 3) Call (and await) getToken() to get the token: `const token = await getToken()`
+ * 
+ * @param query The URL to fetch data from.
+ * @param token The token to be used in the Authorization header.
+ * @returns The JSON response from the fetch() call (no need to do response.json()).
  * */
 export async function fetchWithToken(query: string, { token }: { token: string | null }) {
 	if (!token) throw new Error('Token is required to fetch data.')
