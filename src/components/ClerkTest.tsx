3cff88f493efa7e981d3ed58301f50952bdc83eb
Kazz <i94773007@gmail.com>
2025-03-20 13:08:20 +0100

    changed backend url magic strings into environment variables

--- src/components/ClerkTest.tsx
+++ src/components/ClerkTest.tsx
@@ -1,15 +1,17 @@
 import { useAuth } from '@clerk/astro/react'
 import { useEffect, useState } from 'react'
 
-export default function ClerkTest() {
+const BACKEND_URL = import.meta.env.PUBLIC_BACKEND_URL
 
+export default function ClerkTest() {
+	
 	const { getToken } = useAuth()
 	const [user, setUser] = useState(null)
-
+	
 	useEffect(() => {
 		const fetchUser = async () => {
 			const token = await getToken()
-			const user = await fetch('https://url-shortener-yhpv.onrender.com/auth/me', {
+			const user = await fetch(`${BACKEND_URL}/auth/me`, {
 				headers: { Authorization: `Bearer ${token}` },
 			}).then((res) => res.json())
 			setUser(user)
