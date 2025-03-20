20e8b1658f6d6c82f05401fa703a316d57e46599
Kazz <i94773007@gmail.com>
2025-03-20 14:28:11 +0100

    prog in Dashboard prototyping

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,46 +1,40 @@
 import './Dashboard.css'
 
-import { useAuth } from '@clerk/astro/react'
-import { useEffect, useState } from 'react'
-import { fetchWithToken } from '../../utils/fetch'
-
-const BACKEND_URL = import.meta.env.PUBLIC_BACKEND_URL
+import { useRef } from 'react'
+import UrlListItem from './UrlListItem'
 
 export default function Dashboard() {
-	const { getToken } = useAuth()
-	const [user, setUser] = useState(null)
+	const dashboardContainerRef = useRef<HTMLDivElement>(null)
 
-	useEffect(() => {
-		const handleFetch = async () => {
-			const token = await getToken()
-			try {
-				console.log('fetchin')
-				const user = await fetchWithToken(`${BACKEND_URL}/auth/me`, { token })
-				setUser(user)
-				console.log('fetched user', user)
-			} catch (error) {
-				console.error('Error fetching user:', error)
-			}
-		}
-		handleFetch()
-	}, [])
+	const handleSelectUrl = (urlData: { shortUrl: string, longUrl: string }) => {
+		console.log('selected url:', urlData)
+		dashboardContainerRef.current?.classList.add('open')
+	}
 
 	// Add the class 'open' to the <section> to show the url-info
-	return <section className='dashboard-container'>
-		<div className='urls-list'>
+	return <section className='dashboard-container' ref={dashboardContainerRef}>
+		<div className='urls-list-container'>
 			<h2>
-				List of URLs goes here
+				&nbsp;Your URLs
 			</h2>
+			<div className='urls-list'>
+				<UrlListItem
+					shortUrl='https://smol.reibal.dev/asdf123'
+					longUrl='https://www.example.com/something/longUrlData/query=user%20%searched%20%this&moreData=long_info258260'
+					onClick={handleSelectUrl}
+				/>
+			</div>
 		</div>
-		<div className='url-info'>
+		<div className='url-info-container'>
 			<h2>
-				Information on selected URL goes here
+				&nbsp;Information on selected URL
 			</h2>
-
-			<h3>User Data:</h3>
-			<pre>
-				{JSON.stringify(user, null, 2)}
-			</pre>
+			<br />
+			<h3>Shortened URL:</h3>
+			<p className='url'>https://smol.reibal.dev/asdf123</p>
+			<br />
+			<h3>Long Url</h3>
+			<p className='url'>https://www.example.com/something/longUrlData/query=user%20%searched%20%this&moreData=long_info258260</p>
 		</div>
 	</section>
 }
